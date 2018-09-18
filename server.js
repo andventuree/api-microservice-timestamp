d8aa65c5285a64e902281ddcd965904a974c7541
andventuree <awwong92@gmail.com>
2018-09-18 16:49:28 -0400

    have server running and initial API responses working

--- server.js
+++ server.js
@@ -4,9 +4,19 @@
 // init project
 var express = require('express');
 var app = express();
+let bodyParser = require('body-parser');
+
+// comment out these two lines when ready to be deployed
+// necessary b/c port keeps changing when server is restarted
+let developmentPort = 3000;
+process.env.PORT = developmentPort;
+
+// parses HTTP encodings
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,12 +29,16 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get('/api/timestamp/:date_string', (req, res) => {
+  console.log('req.params: ', req.params);
+  console.log('req.params.date_string: ', req.params.date_string);
+  res.json({message: 'random message'});
+});
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
